14734c596ef3422d86898cffacdea015d8305dcd
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-25 16:29:05 -0500

    create

--- JenkinsfileUpdate.groovy
+++ JenkinsfileUpdate.groovy
@@ -65,7 +65,7 @@ pipeline{
         }
         stage("Build Image"){
             steps{
-              //sh "packer build update/update.json"
+                sh'packer build -var "region=${REGION}" update/update.json'
              echo "Hello"
          }
      }
